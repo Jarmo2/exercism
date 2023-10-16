b6f70524b2874a5d06f4bc13263bdc825a8d8fdf
jarmo.k.kruse <jarmo.k.kruse@pwc.com>
2023-10-16 15:30:00 +0200

    implementing feedback from isaac

--- python/robot-simulator/robot_simulator.py
+++ python/robot-simulator/robot_simulator.py
@@ -5,36 +5,42 @@ NORTH = 'north'
 WEST = 'west'
 SOUTH = 'south'
 
-directions_dict = {NORTH: {'R': EAST, 'L': WEST}, 
-              EAST: {'R': SOUTH, 'L': NORTH},
-              SOUTH: {'R': WEST, 'L': EAST},
-              WEST: {'R': NORTH, 'L': SOUTH}
-             }
+TURN_RIGHT = 'R'
+TURN_LEFT = 'L'
+DIRECTIONS = {
+    NORTH: {TURN_RIGHT: EAST, TURN_LEFT: WEST},
+    EAST: {TURN_RIGHT: SOUTH, TURN_LEFT: NORTH},
+    SOUTH: {TURN_RIGHT: WEST, TURN_LEFT: EAST},
+    WEST: {TURN_RIGHT: NORTH, TURN_LEFT: SOUTH},
+}
+
+OFFSET = {
+    NORTH: (0, 1),
+    EAST: (1, 0),
+    SOUTH: (0, -1),
+    WEST: (-1, 0),
+}
+
 
 class Robot:
-    def __init__(self, direction=NORTH, x_pos=0, y_pos=0):
+    def __init__(self, direction: str = NORTH, x_pos: int = 0, y_pos: int = 0):
         self.direction = direction
         self.x_pos = x_pos
         self.y_pos = y_pos
-        self.coordinates = (self.x_pos, self.y_pos)
+
+    @property
+    def coordinates(self):
+        return (self.x_pos, self.y_pos)
 
 
-    def move(self, direction_str: str):
-        for element in direction_str:
+    def move(self, move_request: str) -> None:
+        for element in move_request:
             if element != 'A':
-                self.direction = directions_dict[self.direction][element]
-            if element == 'A':
-                if self.direction == EAST:
-                    self.x_pos += 1
-                if self.direction == WEST:
-                    self.x_pos -= 1
-                if self.direction == NORTH:
-                    self.y_pos += 1
-                    print('executed')
-                if self.direction == SOUTH:
-                    self.y_pos -= 1
-            self.coordinates = (self.x_pos, self.y_pos)
+                self.direction = DIRECTIONS[self.direction][element]
+            else:
+                offset = OFFSET[self.direction]
+                self.combine(offset)
 
-        
-                
-                
+    def combine(self, offset: tuple) -> None:
+        self.x_pos += offset[0]
+        self.y_pos += offset[1]
